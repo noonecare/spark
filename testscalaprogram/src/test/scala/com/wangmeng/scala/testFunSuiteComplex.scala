4cf36d58c69c21f625c4e91f4dae0a9c56a9fe3e
王蒙 <wangmeng@gridsum.com>
2017-03-25 07:48:35 +0800

    使用 FunSuite 做单元测试

---
+++ testscalaprogram/src/test/scala/com/wangmeng/scala/testFunSuiteComplex.scala
@@ -0,0 +1,20 @@
+package com.wangmeng.scala
+
+import org.scalatest.FunSuite
+
+
+/**
+  * Created by wangmeng on 2017/3/25.
+  * 使用 FunSuite 做单元测试
+  */
+class testFunSuiteComplex extends FunSuite{
+    val testObject = new Complex(1, 2)
+    val other = new Complex(3, 4)
+    val result = new Complex(4, 6)
+    
+    // FunSuite 的特色是，不是定义函数，而是用现成的 test 方法定义， test 方法的第一个参数，是字符串描述这个 test 的用
+    // 意，第二个参数是 assert 语句
+    test("Complex(1, 2) add Complex(2, 4) should be equal to Complex(4, 6)")  {
+        assert( (testObject + other) equals result )
+    }
+}
